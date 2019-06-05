84ce37dfeabcc8f7e3370c88c7223cbec6ea8ac5
Ali <marjanin@usc.edu>
2019-06-04 17:18:29 -0700

    checkpoint

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # add stiffness too
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy import signal
 from all_functions import *
 
 
@@ -30,15 +31,35 @@ def gen_features_fcn(prev_reward, **kwargs):
 
 
 def feat_to_kinematics_fcn(features):
+	each_feature_length = 200 # each cycle duration in seconds/timestep
 	number_of_features = features.shape[0]
 	feat_angles = np.linspace(0, 2*np.pi*(number_of_features/(number_of_features+1)), number_of_features)
 	q0_raw = features*np.sin(feat_angles)
 	q1_raw = features*np.cos(feat_angles)
 	q0_scaled = (q0_raw*np.pi/3)
 	q1_scaled = (q1_raw*np.pi/4)+(np.pi/4)
+	q0_scaled_extended = np.append(q0_scaled, q0_scaled[0])
+	q1_scaled_extended = np.append(q1_scaled, q1_scaled[0])
 
+	q0_scaled_extended_long = np.array([])
+	q1_scaled_extended_long = np.array([])
+	for ii in range(features.shape[0]):
+		q0_scaled_extended_long = np.append(q0_scaled_extended_long, np.linspace(q0_scaled_extended[ii], q0_scaled_extended[ii+1], each_feature_length))
+		q1_scaled_extended_long = np.append(q1_scaled_extended_long, np.linspace(q1_scaled_extended[ii], q1_scaled_extended[ii+1], each_feature_length))
+	q0_scaled_extended_long_3 = np.concatenate([q0_scaled_extended_long[:-1], q0_scaled_extended_long[:-1], q0_scaled_extended_long])
+	q1_scaled_extended_long_3 = np.concatenate([q1_scaled_extended_long[:-1], q1_scaled_extended_long[:-1], q1_scaled_extended_long])
+
+	fir_filter_length = int(np.round(each_feature_length/2))
+	b=np.ones(fir_filter_length,)/fir_filter_length
+	a=1
+	q0_filtered_3 = signal.filtfilt(b, a, q0_scaled_extended_long_3)
+	q1_filtered_3 = signal.filtfilt(b, a, q1_scaled_extended_long_3)
+
+	q0_filtered = q0_filtered_3[q0_scaled_extended_long.shape[0]:2*q0_scaled_extended_long.shape[0]+1]
+	import pdb; pdb.set_trace()
 	plt.figure()
 	plt.plot(q0_scaled, q1_scaled)
+	plt.plot(.9*q0_filtered_3, .9*q1_filtered_3)
 	plt.xlabel = "q0"
 	plt.ylabel = "q1"
 	plt.show(block=True)
@@ -56,6 +77,6 @@ new_features = gen_features_fcn(100, prev_features=np.ones(10,))
 print(new_features)
 [q0_scaled, q1_scaled] = feat_to_kinematics_fcn(new_features)
 
-#import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 input("press anykey to exit")
 #import pdb; pdb.set_trace()
