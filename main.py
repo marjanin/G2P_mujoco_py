5450a42880a3f2679dac92bdf618e10c8b1b5ad9
Ali <marjanin@usc.edu>
2019-06-10 00:18:31 -0700

    put a run_mode for main
    put a run_mode for main
    
    Also, some minor error were fixed on the previous commit such as:
    fixing the number of activation samples in kinematics_activations_show_fcn when only called with activations argument
    
    predefining the size of chassis_pos so that it doesn't need to be appended on each for loop runs (now it is faster)

--- main.py
+++ main.py
@@ -14,21 +14,24 @@ cum_activations = babbling_activations
 pickle.dump([model,cum_kinematics, cum_activations],open("results/mlp_model.sav", 'wb'))
 [model,cum_kinematics, cum_activations] = pickle.load(open("results/mlp_model.sav", 'rb')) # loading the model
 
-# [model, errors, cum_kinematics, cum_activations] =\
-# in_air_adaptation_fcn(
-# 	model=model,
-# 	babbling_kinematics=babbling_kinematics,
-# 	babbling_activations=babbling_activations,
-# 	number_of_refinements=10,
-# 	Mj_render=False)
+run_mode=1
 
-[best_reward, all_rewards] = \
-learn_to_move_fcn(
-	model=model,
-	cum_kinematics=cum_kinematics,
-	cum_activations=cum_activations,
-	refinement=False,
-	Mj_render=True)
+if run_mode==1:
+	[model, errors, cum_kinematics, cum_activations] =\
+	in_air_adaptation_fcn(
+		model=model,
+		babbling_kinematics=babbling_kinematics,
+		babbling_activations=babbling_activations,
+		number_of_refinements=10,
+		Mj_render=False)
+else:
+	[best_reward, all_rewards] = \
+	learn_to_move_fcn(
+		model=model,
+		cum_kinematics=cum_kinematics,
+		cum_activations=cum_activations,
+		refinement=False,
+		Mj_render=True)
 
 input("End of the simulation; press anykey to exit")
 #import pdb; pdb.set_trace()
